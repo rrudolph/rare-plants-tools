3304f20b2013555c19990d9af8e4bfe56d93266e
Rocky Rudolph <rockne@gmail.com>
2022-09-15 11:52:53 -0700

    smal updates to excel script

--- build fields from excel.py
+++ build fields from excel.py
@@ -1,10 +1,34 @@
+'''
+Automated building fields for shared rare plant database for California Channel Islands.
+
+R. Ruddolph
+9/15/22
+
+
+Add field syntax
+# in_table
+# field_name
+# field_type
+# {field_precision}
+# {field_scale}
+# {field_length}
+# {field_alias}
+# {field_is_nullable}
+# {field_is_required}
+# {field_domain}
+
+
+'''
+
+
+
 print("Importing modules")
 # import arcpy
 import yaml, pathlib, arcpy
 from os.path import join
 import pandas as pd
 from icecream import ic
-import numpy as np
+
 
 xlsx = r"C:\GIS\Projects\CHIS_RarePlants\rare-plants-tools\ICBC Rare Plant Fields.xlsx"
 sheet = "NewFieldsAutomated"
@@ -13,12 +37,6 @@ ws = r"C:\GIS\Projects\CHIS_RarePlants\Data Entry Tools\scratch.gdb"
 fc = "brand_newgotime2"
 print("Making featureclass")
 arcpy.management.CreateFeatureclass(ws, fc, "POLYGON", None, "DISABLED", "DISABLED", 'PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]];-20037700 -30241100 10000;-100000 10000;-100000 10000;0.001;0.001;0.001;IsHighPrecision', '', 0, 0, 0, '')
-
-def convert_nan(val):
-    if val == np.nan:
-        return "Value is not a number"
-    else:
-        return val
 
 df = pd.read_excel(xlsx, sheet_name=sheet, header=0)
 df = df.where(pd.notnull(df), None)
@@ -48,27 +66,3 @@ for index, row in df.iterrows():
         None)
 
 
-# for field_dict in dict_list:
-#     for field_name, field_data in field_dict.items():
-#         print("Processing ", field_name, field_data)
-#         new_name = field_data[0]
-#         field_type = field_data[1]
-#         field_precision = field_data[2]
-#         field_scale = field_data[3]
-#         field_length = field_data[4]
-#         field_alias = field_data[5]
-#         field_is_nullable = field_data[6]
-#         field_is_required = field_data[7]
-#         field_domain = field_data[8]
-
-#         arcpy.management.AddField(join(ws, fc), 
-#             new_name,
-#             field_type,
-#             field_precision,
-#             field_scale,
-#             field_length,
-#             field_alias,
-#             field_is_nullable,
-#             field_is_required,
-#             field_domain)
-#         
