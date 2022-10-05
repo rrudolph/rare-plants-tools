31ceb8c9072a411caf64367fca076f646d51df8b
Rocky Rudolph <rockne@gmail.com>
2022-10-05 10:22:13 -0700

    added domain builder code

--- build fields from excel.py
+++ build fields from excel.py
@@ -39,16 +39,29 @@ def convert_field_type(field_type):
     }
     return lookup.get(field_type, "Error") 
 
-xlsx = r"C:\GIS\Projects\CHIS_RarePlants\rare-plants-tools\ICBC Rare Plant Fields.xlsx"
+xlsx = r"C:\GIS\Projects\CHIS_RarePlants\rare-plants-tools\ICBC Rare Plant Fields Template.xlsx"
 sheet = "NewFieldsAutomated"
 
 ws = r"C:\GIS\Projects\CHIS_RarePlants\Data Entry Tools\scratch.gdb"
-fc = "brand_new_fc_sp_feet6"
+fc = "brand_new_fc_sp_feet7"
 out_fc_path = join(ws, fc)
+
+## Convert sheets to domains
+df = pd.read_excel(xlsx, sheet_name=None, header=0)
+for sheet_name, data in df.items():
+    print(sheet_name)
+    print("-------")
+    print(data)
+    if sheet_name.startswith("dom_"):
+        print(f"Adding domain {sheet_name}")
+        arcpy.management.CreateDomain(ws, sheet_name, sheet_name, "TEXT", "CODED")
+        for index, row in data.iterrows():
+            val =  row['Value']
+            print(f"Adding value {val}")
+            arcpy.AddCodedValueToDomain_management(ws, sheet_name, val, val)
 
 # what coordinate system do you want? https://pro.arcgis.com/en/pro-app/latest/arcpy/classes/pdf/projected_coordinate_systems.pdf
 sr = arcpy.SpatialReference(2229) # NAD_1983_StatePlane_California_V_FIPS_0405_Feet
-
 
 print("Making featureclass")
 arcpy.management.CreateFeatureclass(ws, fc, "POLYGON", None, "DISABLED", "DISABLED", sr, '', 0, 0, 0, '')
@@ -58,13 +71,13 @@ df = df.where(pd.notnull(df), None)
 
 for index, row in df.iterrows():
     field_name =  row['FieldName']
-    field_type = row['FieldType']
+    field_type = convert_field_type(row['FieldType'])
     field_length =  row['FieldLength'] 
     field_alias = row['Alias']
     required = True if row['Required'] else False
 
     ic(field_name,
-        convert_field_type(field_type),
+        field_type,
         field_length,
         field_alias,
         required)
@@ -72,7 +85,7 @@ for index, row in df.iterrows():
     print("Making field")
     arcpy.management.AddField(out_fc_path, 
         field_name,
-        convert_field_type(field_type),
+        field_type,
         None,
         None,
         field_length,
